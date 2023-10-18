410df0661e7f3ec14de1509f4bc392b116bb1865
AliAmet <ametov1ali@gmail.com>
2023-10-18 16:19:47 +0600

    First Commit

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99;
+        int h = 104;
+        int a = 97;
+        int r = 114;
+        char c1 = (char)(c);
+        char h1 = (char)(h);
+        char a1 = (char)(a);
+        char r1 = (char)(r);
+        System.out.println(c1 + "," + h1 + "," + a1 + "," + r1);
     }
 }
